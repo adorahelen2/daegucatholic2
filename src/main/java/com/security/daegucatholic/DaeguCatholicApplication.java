a9d8920df3c398c7628bb257af09211f90f94ec5
adorahelen <adorahelenmin@gmail.com>
2024-12-13 21:19:01 +0900

    Initial commit

---
+++ src/main/java/com/security/daegucatholic/DaeguCatholicApplication.java
@@ -0,0 +1,13 @@
+package com.security.daegucatholic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaeguCatholicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DaeguCatholicApplication.class, args);
+    }
+
+}
